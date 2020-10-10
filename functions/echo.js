18bcca58bd49e886be54751ba934a5e28ad1b093
yerx <yer.x.xiong@gmail.com>
2020-10-10 09:32:54 -0400

    added echo function to display the query paramters

---
+++ functions/echo.js
@@ -0,0 +1,10 @@
+exports.handler = async (event) => {
+  console.log(event.queryStringParameters);
+
+  const { text } = event.queryStringParameters;
+
+  return {
+    statusCode: 200,
+    body: `You said: ${text}`,
+  };
+};
