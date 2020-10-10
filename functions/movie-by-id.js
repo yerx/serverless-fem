f234fdff4c08481228f171bf354852584e22713d
yerx <yer.x.xiong@gmail.com>
2020-10-10 12:17:48 -0400

    successfully calling the omdb api to fetch movie data

--- functions/movie-by-id.js
+++ functions/movie-by-id.js
@@ -2,7 +2,6 @@ const movies = require("../data/movies.json");
 
 exports.handler = async ({ queryStringParameters }) => {
   const { id } = queryStringParameters;
-  console.log("id", id);
   const movie = movies.find((movie) => movie.id === id);
 
   if (!movie) {
--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,43 @@
+require("dotenv").config();
+const { URL } = require("url");
+const fetch = require("node-fetch");
+
 const movies = require("../data/movies.json");
 
 exports.handler = async () => {
+  const api = new URL("https://www.omdbapi.com/");
+
+  // add secret api key
+  api.searchParams.set("apikey", process.env.OMDB_API_KEY);
+
+  // https://www.learnwithjason.dev/blog/keep-async-await-from-blocking-execution/
+  /*
+  const promises and promise.all is creating an array of promises that will execute the api fetch in parallel and wait for all of the api fetch to be completed before going to the next step
+  */
+  const promises = movies.map((movie) => {
+    api.searchParams.set("i", movie.id);
+
+    return fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const scores = data.Ratings;
+        console.log("data", data);
+
+        console.log("data with ratings", scores);
+        // once you get the scores return all of the movies with their scores
+        // console.log("movies", { ...movies, scores });
+        return {
+          ...movie,
+          scores,
+        };
+      });
+  });
+
+  // return the movies with ratings
+  const moviesWithRatings = await Promise.all(promises);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(movies),
+    body: JSON.stringify(moviesWithRatings),
   };
 };
